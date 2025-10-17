ae43cee432dad8a6a8b9be0dd3f6f3b2dc50244b
Kostadin Draganov <kostadindraganov@gmail.com>
2025-10-18 02:35:10 +0300

    FIX: fix all errors

--- src/ui/modules/HomePortfolio.tsx
+++ src/ui/modules/HomePortfolio.tsx
@@ -23,7 +23,7 @@ gsap.registerPlugin(ScrollTrigger, CustomEase)
 CustomEase.create('hop', '0.9, 0, 0.1, 1')
 
 export default function Home() {
-	const tagsRef = useRef(null)
+	const tagsRef = useRef<HTMLDivElement>(null)
 	const [showPreloader, setShowPreloader] = useState(isInitialLoad)
 	const [loaderAnimating, setLoaderAnimating] = useState(false)
 	const lenis = useLenis()
@@ -112,8 +112,9 @@ export default function Home() {
 			tl.to('.divider', {
 				scaleY: '100%',
 				duration: 1,
-				onComplete: () =>
-					gsap.to('.divider', { opacity: 0, duration: 0.3, delay: 0.3 }),
+				onComplete: () => {
+					gsap.to('.divider', { opacity: 0, duration: 0.3, delay: 0.3 })
+				},
 			})
 
 			tl.to('#word-1 h1', {
--- src/ui/modules/RichtextModule/AnchoredHeading.tsx
+++ src/ui/modules/RichtextModule/AnchoredHeading.tsx
@@ -6,20 +6,26 @@ export default function AnchoredHeading({
 	children,
 	value,
 }: {
-	as: React.ElementType
+	as: React.ElementType<{
+		children?: React.ReactNode
+		id?: string
+		className?: string
+	}>
 } & PortableTextComponentProps<PortableTextBlock>) {
 	const id = slug(value.children.reduce((acc, { text }) => acc + text, ''))
 
 	return (
 		<Tag id={id} className="group">
-			{children}
+			<>
+				{children}
 
-			<a
-				className="anim-fade-to-r ms-2 no-underline! group-target:inline-block md:hidden md:group-hover:inline-block"
-				href={`#${id}`}
-			>
-				<span className="text-ink/25 inline-block">¶</span>
-			</a>
+				<a
+					className="anim-fade-to-r ms-2 no-underline! group-target:inline-block md:hidden md:group-hover:inline-block"
+					href={`#${id}`}
+				>
+					<span className="text-ink/25 inline-block">¶</span>
+				</a>
+			</>
 		</Tag>
 	)
 }
